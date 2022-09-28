0da1392fc436940d120c6e36acc1ea49fa5a4e05
helo17 <heloise.glorifet@gmail.com>
2022-09-28 17:27:23 +0200

    code main activity + pseudo

--- app/src/main/java/com/example/quizzditch/MainActivity.java
+++ app/src/main/java/com/example/quizzditch/MainActivity.java
@@ -6,10 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button btn_dem;
+    private EditText saisie_pseudo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btn_dem = findViewById(R.id.btn_dem);
+        saisie_pseudo = findViewById(R.id.saisie_pseudo);
     }
     public void bnt_demarrer (View v){
         Intent intent = new Intent(v.getContext(), page_pseudo.class);
--- app/src/main/java/com/example/quizzditch/page_pseudo.java
+++ app/src/main/java/com/example/quizzditch/page_pseudo.java
@@ -3,12 +3,17 @@ package com.example.quizzditch;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class page_pseudo extends AppCompatActivity {
+
+    private Button btn_val_pseudo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page_pseudo);
+
+        btn_val_pseudo = findViewById(R.id.btn_val_pseudo);
     }
 }
