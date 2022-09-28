2dd6ff48f1f85de77e311e0262e69edbb16a7f74
helo17 <heloise.glorifet@gmail.com>
2022-09-28 17:57:53 +0200

    code

--- app/src/main/java/com/example/quizzditch/page_pseudo.java
+++ app/src/main/java/com/example/quizzditch/page_pseudo.java
@@ -4,10 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class page_pseudo extends AppCompatActivity {
 
     private Button btn_val_pseudo;
+    private EditText saisie_pseudo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +17,6 @@ public class page_pseudo extends AppCompatActivity {
         setContentView(R.layout.activity_page_pseudo);
 
         btn_val_pseudo = findViewById(R.id.btn_val_pseudo);
+        saisie_pseudo = findViewById(R.id.saisie_pseudo);
     }
 }
