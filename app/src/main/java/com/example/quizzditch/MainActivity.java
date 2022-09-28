875b48fd94349c121144e548ed79af68d7b51b99
helo17 <heloise.glorifet@gmail.com>
2022-09-28 16:24:53 +0200

    code main activity

--- app/src/main/java/com/example/quizzditch/MainActivity.java
+++ app/src/main/java/com/example/quizzditch/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.quizzditch;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,5 +17,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btn_dem = findViewById(R.id.btn_dem);
+    }
+    public void bnt_demarrer (View v){
+        Intent intent = new Intent(v.getContext(), page_pseudo.class);
+        startActivity(intent);
     }
 }
