e16bb178167e87eaff48e092a97e7ede815dd3e6
helo17 <heloise.glorifet@gmail.com>
2022-10-20 17:44:06 +0200

    avancement

--- app/src/main/java/com/example/quizzditch/P_pseudo.java
+++ app/src/main/java/com/example/quizzditch/P_pseudo.java
@@ -1,9 +1,11 @@
 package com.example.quizzditch;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -39,6 +41,36 @@ public class P_pseudo extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart(); // ALWAYS call first the parent’s method!
+        Log.d(MainActivity.TAG, "OnStart");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause(); // ALWAYS call first the parent’s method!
+        Log.d(MainActivity.TAG, "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop(); // ALWAYS call first the parent’s method!
+        Log.d(MainActivity.TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy(); // ALWAYS call first the parent’s method
+        Log.d(MainActivity.TAG, "onDestroy");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart(); // ALWAYS call first the parent’s method!
+        Log.d(MainActivity.TAG, "onRestart");
     }
 
 }
--- app/src/main/java/com/example/quizzditch/Page_Q1.java
+++ app/src/main/java/com/example/quizzditch/Page_Q1.java
@@ -1,9 +1,11 @@
 package com.example.quizzditch;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -143,4 +145,10 @@ public class Page_Q1 extends AppCompatActivity {
             D++;
         }
     }
+    @Override
+    protected void onStart() {
+        super.onStart(); // ALWAYS call first the parent’s method!
+        Log.d(MainActivity.TAG, "OnStart");
+    }
+
 }
